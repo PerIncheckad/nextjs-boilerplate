aacd166c5fdb57ef578f3bf889757983e25db878
PerIncheckad <per@incheckad.se>
2025-08-25 10:31:58 +0200

    Update supabaseclient.ts

--- lib/supabaseclient.ts
+++ lib/supabaseclient.ts
@@ -1,12 +1,8 @@
-// lib/supabaseClient.ts
+// lib/supabaseclient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
-
-if (!url || !anon) {
-  throw new Error('Saknar NEXT_PUBLIC_SUPABASE_URL eller NEXT_PUBLIC_SUPABASE_ANON_KEY');
-}
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(url, anon);
 export default supabase;
