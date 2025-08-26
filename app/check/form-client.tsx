45c9e42c1bd2e4f01306444a74ec9751a329b6a5
PerIncheckad <per@incheckad.se>
2025-08-26 08:48:53 +0200

    Update form-client.tsx

--- app/check/form-client.tsx
+++ app/check/form-client.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { FormEvent, useEffect, useRef, useState } from 'react';
-import supabase from '../../lib/supabaseclient';
+import supabase from '@/lib/supabaseclient';
 
 
 type SaveStatus = 'idle' | 'saving' | 'done' | 'error';
