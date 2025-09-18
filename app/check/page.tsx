bcb0990430acc353bb25a386751056e23c038265
PerIncheckad <per@incheckad.se>
2025-09-18 13:59:52 +0200

    Pass showTestButtons prop to FormClient component

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -1,11 +1,12 @@
 // app/check/page.tsx
 import FormClient from './form-client';
 import LoginGate from '@/components/LoginGate';
+const showTestButtons = (process.env.NEXT_PUBLIC_SHOW_TEST_BUTTONS ?? 'false') === 'true';
 
 export default function Page() {
   return (
     <LoginGate>
-      <FormClient />
+<FormClient showTestButtons={showTestButtons} />
     </LoginGate>
   );
 }
