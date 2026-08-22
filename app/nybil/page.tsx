5a8eff9c0a613c5419af597cef06e440547edcf4
PerIncheckad <per@incheckad.se>
2026-08-22 10:22:12 +0200

    Remove duplicate page login gate

--- app/nybil/page.tsx
+++ app/nybil/page.tsx
@@ -1,13 +1,8 @@
 // app/nybil/page.tsx
-import LoginGate from '@/components/LoginGate';
 import FormClient from './form-client';
 
 export const dynamic = 'force-dynamic';
 
 export default function NybilPage() {
-  return (
-    <LoginGate>
-      <FormClient />
-    </LoginGate>
-  );
+  return <FormClient />;
 }
