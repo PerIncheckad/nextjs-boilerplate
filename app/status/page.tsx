0f6a42870c86f5432b157c4288c82e9fc5b3d8ed
PerIncheckad <per@incheckad.se>
2026-08-22 10:22:06 +0200

    Remove duplicate page login gate

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,13 +1,8 @@
 // app/status/page.tsx
-import LoginGate from '@/components/LoginGate';
 import FormClient from './form-client';
 
 export const dynamic = 'force-dynamic';
 
 export default function StatusPage() {
-  return (
-    <LoginGate>
-      <FormClient />
-    </LoginGate>
-  );
+  return <FormClient />;
 }
